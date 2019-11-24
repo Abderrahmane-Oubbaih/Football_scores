ead5320fc571dc0a59bd4942a4fb79ac9bce2cdf
youssefataoury <58012785+youssefataoury@users.noreply.github.com>
2019-11-24 05:51:53 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -3,13 +3,7 @@
  <!-- Les Buutons demain hier aujourdHui -->
  <section class="banner">
        <div class="table-responsive container" id="tab">  
-        <!--      <table class="table" border=10 >
-              <tr>
-                <th id="changeth" class="tailleTr"><a id="changea" href="hier.php">Hier</a></th>
-                <th id="changeth" class="tailleTr"><a id="changea" href="aujourdhui.php">Ajourd'hui</a></th>
-                <th id="changeth" class="tailleTr"><a id="changea" href="demain.php">Demain</a></th>
-              </tr>
-        </table> -->
+        
  <ul class="nav nav-pills mb-3 font-weight-bold nav-justified" id="pills-tab" role="tablist">
   <li class="nav-item">
     <a class="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="false">Hier</a>
@@ -29,32 +23,52 @@
 
   <div class="tab-pane fade" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
   
-  <img src="Images/Logos/Laligue.png"  class="mx-auto d-block" id="imgLogoligue">
-
-<table class="table" border=10>
-<tr>
-  <th colspan=3> <h4 class="etat">Etat de matche</h4> </th>
- </tr>
- <tr>
-     <th><img class="imgLogo" src="Images/Logos/bordeaux.png"></th>
-     <th> <h2 class="vs">Vs</h2></th>
-     <th><img class="imgLogo"  src="Images/Logos/bordeaux.png"></th>
- </tr>
-
- <tr>
-     <th> <h2 class="lesequipes">Equipe 1</h2></th>
-     <th></th>
-     <th> <h2 class="lesequipes">Equipe 2</h2></th>
- </tr>
-
- <tr>
-     <th> <h2 class="resu">0</h2></th>
-     <th class="twop">:</th>
-     <th> <h2 class="resu">0</h2></th>
- </tr>
-
-
- </table>
+  <div class="container" style="border: 5px solid #1f7810  ">
+             <div class="text-center">
+              <p style="color:#0060c8 ;font-size:25px;font-weight: bold;">Etat de matche</p>
+           </div>
+           <div class="text-center">
+               <img width="120" height="120" src="Images/Logos/Laligue.png" alt="">
+           </div>
+           <div class="row align-items-center">
+               <div class="col-3 text-center">
+                   <img width="90" height="90" src="Images/Logos/bordeaux.png" alt="">
+               </div>
+               <div class="col-6 text-center">
+                   <p class="my-auto" style="color:#1f7810 ;font-size:30px;font-weight: bold;">VS</p>
+               </div>
+               <div class="col-3  text-center">
+               <img width="90" height="90" src="Images/Logos/bordeaux.png" alt="">
+           </div>
+           </div>
+           
+           <div class="row align-items-center">
+               <div class="col-3  text-center">
+                   <p style="color:white;font-size:20px;font-weight: bold;">Equipe 1</p>
+               </div>
+               <div class="col-6 text-center">
+                   
+               </div>
+               <div class="col-3 text-center">
+                   <p style="color:white;font-size:20px;font-weight: bold;">Equipe 2</p>
+           </div>
+           </div>
+           
+           <div class="row align-items-center">
+               <div class="col-3  text-center">
+                   <p style="color:yellow;font-size:40px;font-weight: bold;">3</p>
+               </div>
+               <div class="col-6 text-center">
+                   
+               </div>
+               <div class="col-3 text-center">
+               <p style="color:yellow;font-size:40px;font-weight: bold;">0</p>
+           </div>
+           </div>
+           
+           
+           
+          </div>
   </div>
 
 
@@ -64,33 +78,57 @@
 
   <div class="tab-pane fade show active" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
 
-  <img src="Images/Logos/Laligue.png"  class="mx-auto d-block" id="imgLogoligue">
-<div>
-<table class="table" border=10 id="centre">
-<tr>
-  <th colspan=3> <h4 class="etat">Etat de matche</h4> </th>
- </tr>
- <tr style="padding: 0;">
-     <th><img class="imgLogo" src="Images/Logos/bordeaux.png"></th>
-     <th> <h2 class="vs">Vs</h2></th>
-     <th><img class="imgLogo"  src="Images/Logos/bordeaux.png"></th>
- </tr>
+  
+  <div class="container" style="border: 5px solid #1f7810  ">
+             <div class="text-center">
+              <p style="color:#0060c8 ;font-size:25px;font-weight: bold;">Etat de matche</p>
+           </div>
+           <div class="text-center">
+               <img width="120" height="120" src="Images/Logos/Laligue.png" alt="">
+           </div>
+           <div class="row align-items-center">
+               <div class="col-3 text-center">
+                   <img width="90" height="90" src="Images/Logos/bordeaux.png" alt="">
+               </div>
+               <div class="col-6 text-center">
+                   <p class="my-auto" style="color:#1f7810 ;font-size:30px;font-weight: bold;">VS</p>
+               </div>
+               <div class="col-3  text-center">
+               <img width="90" height="90" src="Images/Logos/bordeaux.png" alt="">
+           </div>
+           </div>
+           
+           <div class="row align-items-center">
+               <div class="col-3  text-center">
+                   <p style="color:white;font-size:20px;font-weight: bold;">Equipe 1</p>
+               </div>
+               <div class="col-6 text-center">
+                   
+               </div>
+               <div class="col-3 text-center">
+                   <p style="color:white;font-size:20px;font-weight: bold;">Equipe 2</p>
+           </div>
+           </div>
+           
+           <div class="row align-items-center">
+               <div class="col-3  text-center">
+                   <p style="color:yellow;font-size:40px;font-weight: bold;">3</p>
+               </div>
+               <div class="col-6 text-center">
+                   
+               </div>
+               <div class="col-3 text-center">
+               <p style="color:yellow;font-size:40px;font-weight: bold;">0</p>
+           </div>
+           </div>
+           
+           
+           
+          </div>
+           
+            
 
- <tr>
-     <th> <h2 class="lesequipes">Equipe 1</h2></th>
-     <th></th>
-     <th> <h2 class="lesequipes">Equipe 2</h2></th>
- </tr>
 
- <tr>
-     <th> <h2 class="resu">0</h2></th>
-     <th class="twop">:</th>
-     <th> <h2 class="resu">0</h2></th>
- </tr>
-
-
- </table>
- </div>
 </div>
 
 
@@ -98,41 +136,56 @@
 
 <!-- Pour demain -->
   <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-  <img src="Images/Logos/Laligue.png"  class="mx-auto d-block" id="imgLogoligue">
 
-<table class="table" border=10>
-<tr>
-  <th colspan=3> <h4 class="etat">Etat de matche</h4> </th>
- </tr>
- <tr>
-<th colspan=3> <h3 class="etat1"> hh:min jj/mm/aaaa </h3>  </th>
-
- </tr>
- <tr>
-     <th><img class="imgLogo" src="Images/Logos/bordeaux.png"></th>
-     <th> 
-         
-         
-              <h2 class="vs">Vs</h2></th>
-        
-   
-     <th><img class="imgLogo"  src="Images/Logos/bordeaux.png"></th>
- </tr>
-
- <tr>
-     <th> <h2 class="lesequipes">Equipe 1</h2></th>
-     <th></th>
-     <th> <h2 class="lesequipes">Equipe 2</h2></th>
- </tr>
-
- <tr>
-     <th> <h2 class="resu">0</h2></th>
-     <th class="twop">:</th>
-     <th> <h2 class="resu">0</h2></th>
- </tr>
-
-
- </table>
+  <div class="container" style="border: 5px solid #1f7810  ">
+             <div class="text-center">
+              <p style="color:#0060c8 ;font-size:25px;font-weight: bold;">Etat de matche</p>
+           </div>
+           <div class="text-center">
+              <p style="color:red ;font-size:20px;font-weight: bold;">hh:mmm jj/mm/dd</p>
+           </div>
+           <div class="text-center">
+               <img width="120" height="120" src="Images/Logos/Laligue.png" alt="">
+           </div>
+           <div class="row align-items-center">
+               <div class="col-3 text-center">
+                   <img width="90" height="90" src="Images/Logos/bordeaux.png" alt="">
+               </div>
+               <div class="col-6 text-center">
+                   <p class="my-auto" style="color:#1f7810 ;font-size:30px;font-weight: bold;">VS</p>
+               </div>
+               <div class="col-3  text-center">
+               <img width="90" height="90" src="Images/Logos/bordeaux.png" alt="">
+           </div>
+           </div>
+           
+           <div class="row align-items-center">
+               <div class="col-3  text-center">
+                   <p style="color:white;font-size:20px;font-weight: bold;">Equipe 1</p>
+               </div>
+               <div class="col-6 text-center">
+                   
+               </div>
+               <div class="col-3 text-center">
+                   <p style="color:white;font-size:20px;font-weight: bold;">Equipe 2</p>
+           </div>
+           </div>
+           
+           <div class="row align-items-center">
+               <div class="col-3  text-center">
+                   <p style="color:yellow;font-size:40px;font-weight: bold;">3</p>
+               </div>
+               <div class="col-6 text-center">
+                   
+               </div>
+               <div class="col-3 text-center">
+               <p style="color:yellow;font-size:40px;font-weight: bold;">0</p>
+           </div>
+           </div>
+           
+           
+           
+          </div>
   
   </div>
 
