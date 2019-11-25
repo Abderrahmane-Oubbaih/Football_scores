10ff1a9c65d20ffcbd3a1c8c9a388989ffa7b397
wassim fathallah <wassimfathallah11@gmail.com>
2019-11-26 00:34:48 +0100

    Ajouter deux equipes a la table team

--- DATA_BASE/bd_football_scores (1).sql
+++ DATA_BASE/bd_football_scores (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 25 nov. 2019 à 12:27
+-- Généré le :  mar. 26 nov. 2019 à 00:18
 -- Version du serveur :  10.4.8-MariaDB
 -- Version de PHP :  7.1.33
 
@@ -58,24 +58,26 @@ CREATE TABLE `tbl_team` (
 --
 
 INSERT INTO `tbl_team` (`Id`, `Name`, `Logo`) VALUES
-(1, 'Amiens_SC', 'https://www.up-00.com/i/00147/a2c0wn1bhj1u.png'),
-(2, 'Angers_SCO', 'https://www.up-00.com/i/00147/txg6fwabnx0v.png'),
-(3, 'AS_Monaco_FC', 'https://www.up-00.com/i/00147/9c4ujj94pgw4.png'),
-(4, 'As_Saint_Etienne', 'https://www.up-00.com/i/00147/544i8u03sn3s.png'),
-(5, ' Dijon_Football_Cote_Dor', 'https://www.up-00.com/i/00147/ce74nz2ytvb3.png'),
-(6, 'FC_Girondins_De_Bordeaux', 'https://www.up-00.com/i/00147/t1sxyt3anyxs.png'),
-(7, 'FC_Metz	', 'https://www.up-00.com/i/00147/j7skc2m11r83.png'),
-(8, 'FC_Nantes', 'https://www.up-00.com/i/00147/ei03gogdsd0u.png'),
-(9, 'Lille_OSC', 'https://www.up-00.com/i/00147/opkbvwmuuwy7.png'),
-(10, 'Montpellier_HSC	', 'https://www.up-00.com/i/00147/nj5qqc7f7ha3.png'),
-(11, 'Nimes_Olympique', 'https://www.up-00.com/i/00147/vx27wzhxnwfe.png'),
-(12, 'OGC_Nice', 'https://www.up-00.com/i/00147/imjs9o7svv8k.png'),
-(13, 'Olympique_de_Marseille', 'https://www.up-00.com/i/00147/bdyvygxcsx2c.png'),
-(14, 'Olympique_Lyonnais', 'https://www.up-00.com/i/00147/hg9o2rvokjih.png'),
-(15, 'Paris_Saint8Germain_FC', 'https://www.up-00.com/i/00147/glfn3biulbwm.png'),
-(16, 'Stade_Brestois_29', 'https://www.up-00.com/i/00147/nsuez5sxhyss.png'),
-(17, 'Stade_De_Reims	', 'https://www.up-00.com/i/00147/1ynvbrzyk2lp.png'),
-(18, 'Stade_Rennai', 'https://www.up-00.com/i/00147/whfqoeczqdo1.png');
+(1, 'Amiens SC', 'https://www.up-00.com/i/00147/a2c0wn1bhj1u.png'),
+(2, 'Angers SCO', 'https://www.up-00.com/i/00147/txg6fwabnx0v.png'),
+(3, 'AS Monaco FC', 'https://www.up-00.com/i/00147/9c4ujj94pgw4.png'),
+(4, 'As Saint-Étienne', 'https://www.up-00.com/i/00147/544i8u03sn3s.png'),
+(5, ' Dijon Football Côte d\'Or', 'https://www.up-00.com/i/00147/ce74nz2ytvb3.png'),
+(6, 'FC Girondins de Bordeaux', 'https://www.up-00.com/i/00147/t1sxyt3anyxs.png'),
+(7, 'FC Metz	', 'https://www.up-00.com/i/00147/j7skc2m11r83.png'),
+(8, 'FC Nantes', 'https://www.up-00.com/i/00147/ei03gogdsd0u.png'),
+(9, 'Lille OSC', 'https://www.up-00.com/i/00147/opkbvwmuuwy7.png'),
+(10, 'Montpellier HSC	', 'https://www.up-00.com/i/00147/nj5qqc7f7ha3.png'),
+(11, 'Nîmes Olympique', 'https://www.up-00.com/i/00147/vx27wzhxnwfe.png'),
+(12, 'OGC Nice', 'https://www.up-00.com/i/00147/imjs9o7svv8k.png'),
+(13, 'Olympique de Marseille', 'https://www.up-00.com/i/00147/bdyvygxcsx2c.png'),
+(14, 'Olympique Lyonnais', 'https://www.up-00.com/i/00147/hg9o2rvokjih.png'),
+(15, 'Paris Saint-Germain FC', 'https://www.up-00.com/i/00147/glfn3biulbwm.png'),
+(16, 'Stade Brestois 29', 'https://www.up-00.com/i/00147/nsuez5sxhyss.png'),
+(17, 'Stade De Reims	', 'https://www.up-00.com/i/00147/1ynvbrzyk2lp.png'),
+(18, 'Stade Rennais FC 1901', 'https://www.up-00.com/i/00147/whfqoeczqdo1.png'),
+(19, 'Toulouse FC', 'https://www.up-00.com/i/00147/hxg15ydu331e.png'),
+(20, 'RC Strasbourg Alsace', 'https://www.up-00.com/i/00147/aiwpw1t6yk6g.png');
 
 --
 -- Index pour les tables déchargées
