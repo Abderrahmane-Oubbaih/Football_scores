f5da977c425b80647c9a645d47358bee94ca5a64
Hanane Mekkaoui <hanane03.mekkaoui@gmail.com>
2019-11-30 17:58:54 +0000

    update design page Sign In and Add link to hyperlink Sign Up

--- SignIn.php
+++ SignIn.php
@@ -7,16 +7,19 @@
 <body>
 <div class="login-box">
 	
-<h1>Login</h1>
+<h1>Login :</h1>
 <div class="textbox">
 	<i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
 	<input type="text" name="" value="" placeholder="UserName">
 </div>
+<br>
 <div class="textbox">
 	<i class="fa fa-lock" aria-hidden="true"></i>
 	<input type="password" name="" value="" placeholder="PassWord">
 </div>
 <input class="btn" type="button" name="" value="Sign in">
+<br>
+<p>you dont have account <a href="Login/Inscription_Login.php">Sign Up</a> </p>
 </div>
 </body>
 </html>
