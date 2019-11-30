5d9a634690b5723b7800b5a497468fa58b88b340
Hanane Mekkaoui <hanane03.mekkaoui@gmail.com>
2019-11-30 12:24:14 +0000

    design page login

---
+++ SignIn.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign In</title>
+	<link rel="stylesheet" type="text/css" href="Css/StyleSignIn.css">
+</head>
+<body>
+<div class="login-box">
+	
+<h1>Login</h1>
+<div class="textbox">
+	<i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+	<input type="text" name="" value="" placeholder="UserName">
+</div>
+<div class="textbox">
+	<i class="fa fa-lock" aria-hidden="true"></i>
+	<input type="password" name="" value="" placeholder="PassWord">
+</div>
+<input class="btn" type="button" name="" value="Sign in">
+</div>
+</body>
+</html>
