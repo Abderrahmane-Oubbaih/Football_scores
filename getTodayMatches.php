861f00e9222d843f533884b9d88f0dc04b59245d
youssefataoury <58012785+youssefataoury@users.noreply.github.com>
2019-12-03 10:15:36 +0100

    Update getTodayMatches.php

--- getTodayMatches.php
+++ getTodayMatches.php
@@ -40,7 +40,7 @@ function getTodayMatch()
                
                 try
             {
-                $bdd = new PDO('mysql:host=localhost;dbname=football_scores;charset=utf8','root','');    
+                $bdd = new PDO('mysql:host=localhost;dbname=bd_football_scores_v2;charset=utf8','root','');    
             }
             catch(Exception $e)
             {
